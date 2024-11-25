510c9e23ddb4f04e045dae5b6ee68daa94a9cbc5
hiendaika2905@gmail.com <163853441+NguyenMinhHienn@users.noreply.github.com>
2024-11-25 19:30:32 +0700

    update login & register

---
+++ bookstower-main/commons/env.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+define('BASE_URL' , 'http://localhost/BOOKSTOWER/clients/');
+define('BASE_URL_ADMIN' , 'http://localhost/BOOKSTOWER/admin/');
+
+
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'duan1');
+
+define('PATH_ROOT', __DIR__ . '/../');
--- duan1 .sql
+++ duan1 .sql
@@ -379,11 +379,27 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `phone` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `avatar` varchar(255) NOT NULL,
+
   `role` enum('user','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'user',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+
+
+CREATE DATABASE bookstower;
+
+USE bookstower;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+(update login & register)
 --
 -- Dumping data for table `users`
 --
